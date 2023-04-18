92f7a5ef080da9d04183e7dbf8c1a5f499646f6e
Caitlin Cradick <caitlincradick@gmail.com>
2023-04-18 16:47:05 -0700

    Feat/Add updated createGame function, not complete

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-var player1 = createPlayer('Human', '🧑', 0 )
-var player2 = createPlayer('Computer', '💻', 0)
+var player1 = createPlayer('Human', '🧑')
+var player2 = createPlayer('Computer', '💻')
+var gameType= [{name:'classic',icons: 3, winConditions:[]},{name:'classic',icons: 5, winConditions:[]}];
 
-function createPlayer (name, token, wins){
+function createPlayer (name, token){
   var player = {
     name: name,
     token: token,
-    wins: wins 
+    wins: 0 
   }
   console.log(player)
   return player
 }
 
-function createGame(player1, player2) {
-console.log(player1.name)
-}
-
-createGame(player1, player2)
+function createGame(player1, player2, gameType) {
+  var game = {
+    player1: player1, 
+    player2: player2, 
+    gameType:gameType, 
+    data: []  
+  }
+   console.log(game)
+   return game 
+ }
+ 
+ createGame(player1, player2, gameType)
