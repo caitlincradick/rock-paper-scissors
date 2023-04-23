6b9e4e6e9beac8169dcbdc7da426014f24d7ea53
Caitlin Cradick <caitlincradick@gmail.com>
2023-04-22 23:29:16 -0700

    feature/classic fully functional on dom no bugs

--- main.js
+++ main.js
@@ -13,64 +13,47 @@ var choiceComputer = document.getElementById('choiceComputer');
 var classicBtn = document.querySelector('.classic-btn');
 var difficultBtn = document.querySelector('.difficult-btn');
 var iconsView = document.querySelector('.icons');
-var allIcons = document.querySelectorAll('img');
-var icon = document.querySelector('.icon')
-var rock = document.querySelector('#rock');
-var paper = document.querySelector('#paper');
-var scissors = document.querySelector('#scissors');
-var lizard = document.querySelector('#lizard');
-var alien = document.querySelector('#alien');
+var allIcons = document.querySelectorAll('.icon');
+var rock = document.querySelector('.rock');
+var paper = document.querySelector('.paper');
+var scissors = document.querySelector('.scissors');
+var lizard = document.querySelector('.lizard');
+var alien = document.querySelector('.alien');
 
 //EVENT LISTENERS 
 window.addEventListener('load', createGame(gameBoardChoices));
 
 classicBtn.addEventListener('click', function(event) {
-  determineGameType(event);
+  currentGame.gameType = 'classic'
   showClassicGameBoard();
-  // getAllIcons(event)
-  
 });
 
 difficultBtn.addEventListener('click', function(event) {
+  currentGame.gameType = 'difficult'
   showDifficultGameBoard();
-  determineGameType(event);
-  // takeTurnDifficult(event.target.id, difficultIcons);
 });
 
-rock.addEventListener('click', function(event) {
-  console.log('rock clicked')
-  getAllIcons(event)
-  determineGameType(event)
-  takeTurnClassic(event.target.id, classicIcons);
-  displayPlayerChoices()
-})
 
-paper.addEventListener('click', function(event) {
-  getAllIcons(event)
-  determineGameType(event)
-  takeTurnClassic(event.target.id, classicIcons);
-  console.log('paper clicked')
-})
-
-scissors.addEventListener('click', function(event) {
-  getAllIcons(event)
-  takeTurnClassic(event.target.id, classicIcons);
- 
-  console.log('scissors clicked')
-})
+[rock, paper, scissors, alien, lizard].forEach((iconChoice)=>{
+  iconChoice.addEventListener('click', (event)=> {
+console.log('for each')
+if(currentGame.gameType === 'classic'){
+takeTurnClassic(event.target.id, classicIcons);
+} else if (currentGame.gameType === 'difficult') {
+  takeTurnDifficult(event.target.id, difficultIcons)
+}
+  });
+});
 
 changeGameBtn.addEventListener('click', function () {
   backtoHomePage();
 })
 
 //GLOBAL VARIABLES
-var classicIcons = ['rock', 'paper', 'scissors']
+var classicIcons = ['rock', 'paper', 'scissors'];
 var difficultIcons = ['rock', 'paper', 'scissors', 'alien', 'lizard']
 var gameBoardChoices = [];
 var currentGame;
-// var humanChoice;
-var gameType;
-
 
 
 //JS DATA MODEL
@@ -89,58 +72,46 @@ var player2 = createPlayer('Computer', '💻')
   var game = {
     player1:player1, 
     player2:player2, 
-    gameType:gameType, 
+    // gameType:gameType, 
     gameBoardChoices: gameBoardChoices  
   }
   currentGame = game
   return currentGame
 }  
-// createGame(gameBoardChoices)
 
 function getRandomIndex(iconArrays){
   return Math.floor(Math.random() * iconArrays.length);
 }
 
-function getAllIcons(event){
-  for(var i = 0; i < allIcons.length; i++) {
-    allIcons[i] = event.target.id 
-  }
-  return allIcons[i]
-}
-
-function determineGameType(event) {
-var classicOrDifficult = event.target.className
-  if(classicOrDifficult.includes('classic-btn')){
-    console.log('hello')
-    // currentGame.rules = classicRules
-    gameType = 'classic'
-  } else if (classicOrDifficult.includes('difficult-btn')){
-    console.log('hi')
-      // currentGame.rules = difficultRules
-      gameType = 'difficult'
-  }
-  return gameType
-}
+// function getAllIcons(event){
+//   for(var i = 0; i < allIcons.length; i++) {
+//     allIcons[i] = event.target.id 
+//     allIcons[i].addEventListener('click', function(event){
+//       takeTurnClassic(event.target.id, classicIcons);
+//       takeTurnDifficult(event.target.id, difficultIcons)
+//     })
+//   }
+//   return allIcons[i]
+// }
 
 function takeTurnClassic(humanChoice, classicIcons) {
 gameBoardChoices = []
-gameType = 'classic'
-if(currentGame.player1.name === 'Human'){
-  currentGame.player1.choice = humanChoice 
-  gameBoardChoices.push(currentGame.player1.choice)
-} 
-if (currentGame.player2.name === 'Computer') {
-  currentGame.player2.choice= classicIcons[getRandomIndex(classicIcons)]
-  gameBoardChoices.push(currentGame.player2.choice)  
-}
+  if(currentGame.player1.name === 'Human'){
+    currentGame.player1.choice = humanChoice 
+    gameBoardChoices.push(currentGame.player1.choice)
+  } 
+  if (currentGame.player2.name === 'Computer') {
+    currentGame.player2.choice= classicIcons[getRandomIndex(classicIcons)]
+    gameBoardChoices.push(currentGame.player2.choice)  
+  }
 console.log(gameBoardChoices)
-displayPlayerChoices()
+  displayPlayerChoices()
   determineWins()
-  setTimeout(resetGame, 2000)
-}
+  setTimeout(resetGame, 3000)
+  }
 
 function takeTurnDifficult(humanChoice, difficultIcons) {
-  gameBoardChoices = []
+gameBoardChoices = []
   if(currentGame.player1.name === 'Human'){
     currentGame.player1.choice = humanChoice 
     gameBoardChoices.push(currentGame.player1.choice)
@@ -151,19 +122,18 @@ function takeTurnDifficult(humanChoice, difficultIcons) {
   }
   displayPlayerChoices()
   determineWins()
-  setTimeout(resetGame, 2000)
+  setTimeout(resetGame, 3000)
+  
 }
 
-
 function determineRules(){
-  if(gameType === 'classic'){
+  if(currentGame.gameType === 'classic'){
     currentGame.rules = classicRules
-  } else if (gameType === 'difficult') {
+  } else if (currentGame.gameType === 'difficult') {
     currentGame.rules = difficultRules
   }
   return currentGame.rules
 }
-
 
 function determineWins(){
   determineRules()
@@ -181,7 +151,6 @@ function determineWins(){
     gameStatus.innerText = 'Player 2 Wins 💻'
     winsPlayer2.innerText = `${currentGame.player2.wins}`
   }
-  // gameBoardChoices = []
 }
 
 //JS DOM
@@ -240,8 +209,6 @@ function hideIcons(){
   hide(lizard);
 }
 
-
-
 function displayPlayerChoices(){
   for (var i = 0; i < allIcons.length; i++) {
     hide(allIcons[i]);
@@ -254,41 +221,22 @@ function displayPlayerChoices(){
   `;
 }
 
-
 function resetGame(){
+  gameStatus.innerText = 'Choose your fighter !'
   selectedIcons.innerHTML = ''
   show(rock);
   show(paper)
   show(scissors)
-
-  if(gameType === 'difficult')
+// hide(lizard)
+//  hide(alien)
+  console.log('classic reset')
+ if(currentGame.gameType === 'difficult'){
+    console.log('difficult resest')
+    show(rock);
+    show(paper)
+    show(scissors)
   show(lizard)
   show(alien)
 }
-// function resetGame(){
-//  currentGame.player1.choice = ''
-//  currentGame.player2.choice = ''
-//   selectedIcons.innerHTML = ''
-//   gameStatus.innerText = 'Choose your fighter !'
-//   if(gameType === 'classic'){
-//     classicBoard.innerHTML =
-//     `<section class ="classic-board">
-//     <img class="rock icon" id="rock" src="assets/rock.png" alt="drawing of rocks">
-//     <img class="scissors icon" id="scissors" src="assets/scissors.png" alt="drawing of scissors">
-//     <img class="paper icon" id="paper" src="assets/paper.png" alt="drawing of paper">
-//   </section>`
-//   } else {
-//     difficultBoard.innerHTML =
-//     `<section class = "difficult-board">
-//           <img class="rock icon" id="rock" src="assets/rock.png" alt="drawing of rocks">
-//           <img class="scissors icon" id="scissors" src="assets/scissors.png" alt="drawing of scissors">
-//           <img class="paper icon" id="paper" src="assets/paper.png" alt="drawing of paper">
-//           <img class="alien icon" id="alien" src="assets/alien.png" alt="drawing of alien">
-//           <img class="lizard icon" id="lizard" src="assets/lizard.png" alt="drawing of lizard"></img>`
-//   }
-  // iconsView.innerHTML = `
-  // <img class="rock icon " id="rock" src="assets/rock.png" alt="drawing of rocks">
-  // <img class="scissors icon " id="scissors" src="assets/scissors.png" alt="drawing of scissors">
-  // <img class="paper icon " id="paper" src="assets/paper.png" alt="drawing of paper">
-  // <img class="alien icon hidden" id="alien" src="assets/alien.png" alt="drawing of alien">
-  // <img class="lizard icon hidden" id="lizard" src="assets/lizard.png" alt="drawing of lizard">`
+}
+
